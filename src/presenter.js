6bbfbd2a6d57339f5063be26441cdefcc5403fcb
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2025-08-19 00:08:51 -0400

    Multiplicacion Funciona

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,12 @@
 import sumar from "./sumador";
+import multi from "./multi"; // Importa la función multi
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const multButton = document.querySelector("#mult-button"); // Selecciona el botón multiplicar
+const sumarButton = document.querySelector("#sumar-button"); // Selecciona el botón sumar
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,5 +14,12 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  // Si el botón que disparó el evento es el de sumar
+  if (event.submitter === multButton) {
+    div.innerHTML = "<p>" + multi(firstNumber, secondNumber) + "</p>";
+  } 
+
+  if(event.submitter === sumarButton){
+    div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  }
 });
